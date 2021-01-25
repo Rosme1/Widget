25f5a8ffe075226d54e38fdb24b31c9245d174e2
Rosmery <rosarioestrellarosmery@gmail.com>
2021-01-25 16:27:54 -0400

    touch-ups

--- js/main.js
+++ js/main.js
@@ -2,12 +2,8 @@
 window.addEventListener('load', function(){
 
 var table = document.querySelector("#movies");
-var date = new Date();
 
-var hours = date.getHours();
-var minutes = date.getMinutes();
 
-var time = hours + ":" + minutes;
 
 
 getItems()
@@ -25,30 +21,40 @@ function getItems(){
 function itemsList(items){
 
     items.map((item) => {
+        let dateTime = new Date(item.ts);
+        let hours = dateTime.getHours();
+        let minutes = dateTime.getMinutes();
+        let time = hours + ":" + minutes;
+
         let div = document.createElement("div");
         let divText = document.createElement("div");
         let img = document.createElement("img");
         let title = document.createElement("p");
-        let date = document.createElement("p");
+        let date = document.createElement("div");
+        let ico = document.createElement("img");
         let author = document.createElement("p");
-        let hr = document.createElement("hr");
+        let dateText = document.createElement("label");
         
-        
+        dateText.className = "text-date";
         div.className = "item-content";
         divText.className = "text-content";
         img.className = "img";
+        ico.className = "ico";
         title.className = "black";
         date.className = "gray date";
         author.className = "gray author";
 
-        date.innerHTML = "Today at  " + time;
+        dateText.innerHTML = "      Aujourd'hui à " + time;
         title.innerHTML = item.title;
         author.innerHTML = item.author;
         
+        
         img.src = item.img_url;
+        ico.src = "./img/timeIco.png";
        
 
-        
+        date.appendChild(ico);
+        date.appendChild(dateText);
         divText.appendChild(date);
         divText.appendChild(title);
         divText.appendChild(author);
